2de4bc636720cd53f86b8cc244276eaa64f03011
Farkansh Gupta <farkanshgupta@gmail.com>
2025-07-13 22:05:33 +0530

    [Farkansh Gupta] Added:Sorting Algorithms

---
+++ Sorting_Algorithms/MergeSort.java
@@ -0,0 +1,41 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] prices = {250, 199, 300, 150, 275};
+        mergeSort(prices, 0, prices.length - 1);
+
+        System.out.print("Sorted Book Prices: ");
+        for (int price : prices) {
+            System.out.print(price + " ");
+        }
+    }
+
+    static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    static void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) left[i] = arr[low + i];
+        for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = low;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else
+                arr[k++] = right[j++];
+        }
+        while (i < n1) arr[k++] = left[i++];
+        while (j < n2) arr[k++] = right[j++];
+    }
+}
