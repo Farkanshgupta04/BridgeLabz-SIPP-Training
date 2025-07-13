73a908df9851859f0eea78e868f477d82dd5836a
Farkansh Gupta <farkanshgupta@gmail.com>
2025-07-13 22:30:52 +0530

    Delete Linear_and_Binary_Search directory

--- Linear_and_Binary_Search/StringBuilder/ReverseString.java
+++
@@ -1,8 +0,0 @@
-public class ReverseString {
-    public static void main(String[] args) {
-        String input = "hello";
-        StringBuilder sb = new StringBuilder(input);
-        String reversed = sb.reverse().toString();
-        System.out.println("Reversed: " + reversed);
-    }
-}
